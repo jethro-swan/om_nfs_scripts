7e18c3d993e15d11f4d4b9e3981f0deadee6f6d6
John Waters <john.waters@cooptel.net>
2020-06-15 21:24:40 +0100

    Substitute VM hostname for IP

--- vm_nfs_scripts/vm_nfs_setup.sh
+++ vm_nfs_scripts/vm_nfs_setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Identify VM IP address:
-VM_IP=$(hostname -I)
-echo $VM_IP
+# Identify VM hostname:
+VM_HOSTNAME=$(hostname)
+echo $VM_HOSTNAME
 
 SUBNET="192.168.1.0/24"
 echo "If you are using a LAN subnet other than 192.168.1.0/24 you will have to"
@@ -21,12 +21,12 @@ create_bind_point()
 {
     cd /home/om
     git clone https://github.com/jethro-swan/$1
-    mkdir /srv/nfs4/$1.$VM_IP
-    mount --bind /home/om/$1 /srv/nfs4/$1.$VM_IP
+    mkdir /srv/nfs4/$1.$VM_HOSTNAME
+    mount --bind /home/om/$1 /srv/nfs4/$1.$VM_HOSTNAME
     # NFS binds
-    echo "/home/om/$1 /srv/nfs4/$1.$VM_IP none bind 0 0" >> /etc/fstab
+    echo "/home/om/$1 /srv/nfs4/$1.$VM_HOSTNAME none bind 0 0" >> /etc/fstab
     # NFS exports
-    echo "/srv/nfs4/$1.$VM_IP $SUBNET(rw,sync,no_subtree_check)" >> /etc/exports
+    echo "/srv/nfs4/$1.$VM_HOSTNAME $SUBNET(rw,sync,no_subtree_check)" >> /etc/exports
 }
 create_bind_point "openmoney-api"
 create_bind_point "openmoney-network"
