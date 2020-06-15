08ff360e4b13cc3e0d9f0433430a52606286b8f9
John Waters <john.waters@cooptel.net>
2020-06-15 20:25:10 +0100

    Update vm_nfs_setup.sh

--- vm_nfs_scripts/vm_nfs_setup.sh
+++ vm_nfs_scripts/vm_nfs_setup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+  
+VM_IP=$(hostname -I)
+echo $VM_IP
 
-cd /home/om
+SUBNET="192.168.1.0/24"
+echo "If you are using a LAN subnet other than 192.168.1.0/24 you will have to"
+echo "edit  /etc/exports  accordingly."
 
 # Clone the four Open Money repositories:
 git clone https://github.com/jethro-swan/openmoney-api
@@ -8,21 +13,22 @@ git clone https://github.com/jethro-swan/openmoney-network
 git clone https://github.com/jethro-swan/openmoney-gift-api
 git clone https://github.com/jethro-swan/openmoney-gift
 
-sudo apt install nfs-kernel-server
-#sudo cat /proc/fs/nfsd/versions
+apt install nfs-kernel-server
+#cat /proc/fs/nfsd/versions
 
-sudo mkdir -p /srv/nfs4/{openmoney-api,openmoney-network,openmoney-gift-api,openmoney-gift}
-sudo mount --bind /home/om/openmoney-api /srv/nfs4/openmoney-api
-sudo mount --bind /home/om/openmoney-network /srv/nfs4/openmoney-network
-sudo mount --bind /home/om/openmoney-gift-api /srv/nfs4/openmoney-gift-api
-sudo mount --bind /home/om/openmoney-gift /srv/nfs4/openmoney-gift
-
-# The shares are exported for access by other devices on the LAN:
-sudo cat b/nfs_binds >> /etc/fstab
-sudo cat b/nfs_exports >> /etc/exports
-sudo exportfs -ra
-
-echo "If you are using a LAN subnet other than 192.168.1.0/24 you will have to"
-echo "edit  /etc/exports  accordingly."
-
-
+create_bind_point()
+{
+    cd /home/om
+    git clone https://github.com/jethro-swan/$1
+    mkdir /srv/nfs4/$1.$VM_IP
+    mount --bind /home/om/$1 /srv/nfs4/$1.$VM_IP
+    # NFS binds
+    echo "/home/om/$1 /srv/nfs4/$1.$VM_IP none bind 0 0" >> /etc/fstab
+    # NFS exports
+    echo "/srv/nfs4/$1.$VM_IP $SUBNET(rw,sync,no_subtree_check)" >> /etc/exports
+}
+create_bind_point "openmoney-api"
+create_bind_point "openmoney-network"
+create_bind_point "openmoney-gift-api"
+create_bind_point "openmoney-gift"
+exportfs -ra
