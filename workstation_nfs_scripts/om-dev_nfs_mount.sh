e29692a221438b504a8d61b0e19dd14036a12b6a
John Waters <john.waters@cooptel.net>
2020-06-15 21:28:31 +0100

    Substitute VM hostname for IP

--- workstation_nfs_scripts/om-dev_nfs_mount.sh
+++ workstation_nfs_scripts/om-dev_nfs_mount.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if [[ $1 =~ ^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4}$ ]]; then
-    VM_IP=$1
-else
-    echo "$VM_IP is not a valid IPv4 address"
-    echo "Usage: om-dev_nfs_mount.sh <IP address of VM>"
-fi     
+#if [[ $1 =~ ^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4}$ ]]; then
+#    VM_IP=$1
+#else
+#    echo "$VM_IP is not a valid IPv4 address"
+#    echo "Usage: om-dev_nfs_mount.sh <hostname of VM>"
+#fi     
 
 HERE=$(pwd)
-echo "Setting up NFS access to Open Money directories on VM at $VM_IP from $HERE"
+echo "Setting up NFS access to Open Money directories on VM at $VM_HOSTNAME from $HERE"
 
 nfs_mount()
 {
@@ -20,7 +20,7 @@ nfs_mount()
         echo "$HERE/$1 has been created"
     fi
     # Mount VM's OM NFS share:
-    sudo mount -t nfs -o vers=4 $VM_IP:/$1.VM_IP $HERE/$1
+    sudo mount -t nfs -o vers=4 $VM_HOSTNAME:/$1.VM_HOSTNAME $HERE/$1
 }
 nfs_mount "openmoney-api"
 nfs_mount "openmoney-gift"
